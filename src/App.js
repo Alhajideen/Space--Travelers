d5fcbc78f71ab2f1b4f5df27d4647dcb916b7449
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-11-14 23:39:54 +0500

    Create a basic structure for navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Space Travellers</h1>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<h1>Space Travelers Rockets</h1>} />
+        <Route path="/missions" element={<h1>Space Travelers Missions</h1>} />
+        <Route path="/profile" element={<h1>Space Travelers Profile</h1>} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import logo from '../images/logo.png';
+
+const Header = () => (
+  <header>
+    <div>
+      <Link to="/">
+        <img src={logo} alt="logo" />
+        <h1>Space Traverlers&apos; Hub</h1>
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <NavLink
+              to="/"
+              end
+              className={({ isActive }) => (isActive ? 'activelink' : undefined)}
+            >
+              Rockets
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/missions"
+              className={({ isActive }) => (isActive ? 'activelink' : undefined)}
+            >
+              Missions
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/profile"
+              className={({ isActive }) => (isActive ? 'activelink' : undefined)}
+            >
+              My Profile
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+);
+
+export default Header;

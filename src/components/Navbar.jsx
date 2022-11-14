db67ef418b2d041ab080e80c9bbf29de528259ad
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-11-15 00:09:32 +0500

    Add basic styles for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,44 +1,45 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import logo from '../images/logo.png';
+import '../styles/Navbar.css';
 
 const Navbar = () => (
-  <header>
-    <div>
-      <Link to="/">
-        <img src={logo} alt="logo" />
-        <h1>Space Traverlers&apos; Hub</h1>
-      </Link>
-      <nav>
-        <ul>
-          <li>
-            <NavLink
-              to="/"
-              end
-              className={({ isActive }) => (isActive ? 'activelink' : undefined)}
-            >
-              Rockets
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/missions"
-              className={({ isActive }) => (isActive ? 'activelink' : undefined)}
-            >
-              Missions
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/profile"
-              className={({ isActive }) => (isActive ? 'activelink' : undefined)}
-            >
-              My Profile
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    </div>
+  <header className="header-container">
+    <NavLink to="/" className="home-page">
+      <img src={logo} alt="logo" className="logo" />
+      <h1 className="title">Space Traverlers&apos; Hub</h1>
+    </NavLink>
+
+    <nav>
+      <ul className="navlinks">
+        <li className="navItem">
+          <NavLink
+            to="/"
+            end
+            className={({ isActive }) => (isActive ? 'activelink' : undefined)}
+          >
+            Rockets
+          </NavLink>
+        </li>
+        <li className="navItem">
+          <NavLink
+            to="/missions"
+            className={({ isActive }) => (isActive ? 'activelink' : undefined)}
+          >
+            Missions
+          </NavLink>
+        </li>
+        <li className="divider" />
+        <li className="navItem">
+          <NavLink
+            to="/profile"
+            className={({ isActive }) => (isActive ? 'activelink' : undefined)}
+          >
+            My Profile
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
   </header>
 );
 

216aa59f7b4def549922e59c3d1c5981518152ea
Alhajideen <salifunurudeen834@gmail.com>
2022-11-14 15:24:20 -0600

    add a table in MissionContainer.js

---
+++ src/components/Missions/MissionContainer.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Table from 'react-bootstrap/Table';
+
+function MissionContainer() {
+  return (
+    <div>
+      <Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>Missions</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan={2}>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+  );
+}
+
+export default MissionContainer;

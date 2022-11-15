51910ef91f9c324cd3271085e136bd4283c0c944
Alhajideen <salifunurudeen834@gmail.com>
2022-11-14 19:12:21 -0600

    set up a function to change member status

--- src/components/Missions/MissionContainer.js
+++ src/components/Missions/MissionContainer.js
@@ -1,36 +1,55 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Table from 'react-bootstrap/Table';
 import '../../styles/mission.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMissions, joinMission } from '../../redux/Mission/missionSlice';
 
 function MissionContainer() {
+  const dispatch = useDispatch();
+  const missions = useSelector((state) => state.mission.data);
+  useEffect(() => {
+    dispatch(getMissions());
+    setTimeout(() => {
+      // console.log(missions.data);
+    }, 100);
+  }, []);
+
+  const handleMember = (id) => {
+    dispatch(joinMission(id));
+    // console.log(missions);
+  };
   return (
-    <div className="container">
-      <Table striped bordered hover size="lg">
+    <div className="mission-container my-3">
+      <Table striped bordered hover size="sm">
         <thead>
           <tr>
             <th>Missions</th>
-            <th colSpan={4}>Description</th>
+            <th>Description</th>
             <th>Status</th>
             <th> </th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td colSpan={4}>
-              Mark Mark Mark Mark Mark Mark Mark Mark Mark Mark
-            </td>
-            <td>
-              <button type="button" className="not-member p-2">
-                Not a Member
-              </button>
-            </td>
-            <td>
-              <button type="button" className="join rounded-1 p-1">
-                Join Mission
-              </button>
-            </td>
-          </tr>
+          {missions.map((mission) => (
+            <tr key={mission.mission_id} className="table-rows">
+              <td className="mission-name">{mission.mission_name}</td>
+              <td>{mission.description}</td>
+              <td className="status">
+                <button type="button" className="not-member p-1">
+                  Not a Member
+                </button>
+              </td>
+              <td className="status">
+                <button
+                  type="button"
+                  className="join rounded-1 p-1"
+                  onClick={() => handleMember(mission.mission_id)}
+                >
+                  Join Mission
+                </button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </Table>
     </div>
--- src/redux/Mission/missionSlice.js
+++ src/redux/Mission/missionSlice.js
@@ -1,13 +1,58 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const initialState = [0];
+export const getMissions = createAsyncThunk('getMission', async () => {
+  const missions = await axios('https://api.spacexdata.com/v3/missions');
+  // console.log(missions);
+  return missions.data;
+});
+
+const initialState = { loading: false, data: [], error: null };
 
 export const missionSlice = createSlice({
-  name: 'counter',
+  name: 'missions',
   initialState,
   reducers: {
     // Do stuff here
+    joinMission(state, action) {
+      const newMissions = [];
+      console.log(state.data);
+      state.data.map((mission) => {
+        if (mission.id === action.payload) {
+          newMissions.push({ ...mission, member: !mission.member });
+          console.log(mission);
+        } else {
+          newMissions.push(mission);
+        }
+        return newMissions;
+      });
+      // console.log(newMissions);
+    },
+  },
+
+  extraReducers: (Builder) => {
+    Builder.addCase(getMissions.pending, (state) => {
+      const newState = { ...state, loading: true };
+      return newState;
+    })
+      .addCase(getMissions.fulfilled, (state, action) => {
+        const missions = action.payload;
+        const arr = [];
+        missions.forEach((info) => {
+          const myObj = { ...info, member: false };
+          arr.push(myObj);
+        });
+        const newState = { ...state, data: arr, loading: false };
+        console.log(state.data);
+        return newState;
+      })
+      .addCase(getMissions.rejected, (state) => {
+        const newState = { ...state, error: 'Error 404. Failed to fetch' };
+        return newState;
+      });
   },
 });
+
+export const { joinMission } = missionSlice.actions;
 
 export default missionSlice.reducer;

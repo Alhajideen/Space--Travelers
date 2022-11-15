5a02d4b8eaa90b877456d6caba79c995d74b06b7
Alhajideen <salifunurudeen834@gmail.com>
2022-11-15 06:14:28 -0600

    display joined mission in myprofile page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import MissionContainer from './components/Missions/MissionContainer';
 import Navbar from './components/Navbar';
+import Profile from './components/Profile/Profile';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<p>Hello from rockets!</p>} />
         <Route path="/missions" element={<MissionContainer />} />
-        <Route path="/profile" element={<p>Hello from profile!</p>} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
   );
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMissions } from '../../redux/Mission/missionSlice';
 
 function Profile() {
+  const dispatch = useDispatch();
+  const missions = useSelector((state) => state.mission.data);
+  const [mission, setMission] = useState([]);
+  const [empty, setEmpty] = useState(false);
+  useEffect(() => {
+    dispatch(getMissions());
+    const joinedMisssion = missions.filter((item) => item.member === true);
+    setMission(joinedMisssion);
+    if (joinedMisssion.length === 0) {
+      setEmpty(true);
+    }
+  }, [dispatch]);
+  console.log(mission);
   return (
-    <div>Profile</div>
-  )
+    <div>
+      <div className="my-missions">
+        <div className="header-text">
+          <h1>My Missions</h1>
+        </div>
+        <table>
+          <tbody>
+            {!empty
+              && mission.map((item) => (
+                <tr key={item.mission_id}>
+                  <td>{item.mission_name}</td>
+                </tr>
+              ))}
+            {empty && (
+              <tr>
+                <td>You have not joined any mission.</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
-export default Profile
+export default Profile;

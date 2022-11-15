e37885306ecccc6d914f472f37d0c63d8cfcd30d
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-11-15 23:21:12 +0500

    Call fetch rocket action on load

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
+import { fetchRocketsData } from './redux/Rockets/rocketSlice';
 import Navbar from './components/Navbar';
+import Rockets from './components/rockets/Rockets';
 
 function App() {
+  const dispatch = useDispatch();
+  dispatch(fetchRocketsData());
+
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<p>Hello from rockets!</p>} />
+        <Route path="/" element={<Rockets />} />
         <Route path="/missions" element={<p>Hello from missions!</p>} />
         <Route path="/profile" element={<p>Hello from profile!</p>} />
       </Routes>

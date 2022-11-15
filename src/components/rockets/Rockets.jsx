7e64dbc6be6935e956c465c08f8c20be82ff423a
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-11-16 00:38:43 +0500

    Render list of rockets on UI

--- src/components/rockets/Rockets.jsx
+++ src/components/rockets/Rockets.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const Rockets = () => (
-  <div className="rockets">Rockets</div>
-);
+const Rockets = () => {
+  const { rocketsArray, loading } = useSelector((state) => state.rockets);
+  return (
+    <ul className="rockets">
+      {
+        loading ? 'Fetching' : rocketsArray.map((rocket) => (
+          <li key={rocket.id} className="rocket">
+            <img src={rocket.image} alt="Rocket" />
+            <div>{rocket.name}</div>
+            <div>{rocket.desc}</div>
+          </li>
+        ))
+      }
+    </ul>
+  );
+};
 
 export default Rockets;

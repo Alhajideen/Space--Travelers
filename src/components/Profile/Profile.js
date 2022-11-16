c4481c6788f80bead975d9b0f6a8e47324056a05
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-11-16 23:42:16 +0500

    Display reserved rockets in profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,20 +4,25 @@ import '../../styles/profile.css';
 
 const Profile = () => {
   const missions = useSelector((state) => state.mission.data);
+  const rockets = useSelector((state) => state.rockets.rocketsArray);
+  const [reservedRockets, setReservedRockets] = useState([]);
   const [mission, setMission] = useState([]);
   const [empty, setEmpty] = useState(false);
+
   useEffect(() => {
     const joinedMisssion = missions.filter((item) => item.member === true);
     setMission(joinedMisssion);
     if (joinedMisssion.length === 0) {
       setEmpty(true);
     }
+    setReservedRockets(rockets.filter((rocket) => rocket.reserved));
   }, []);
+
   return (
     <div className="profile">
       <div className="my-missions">
         <div className="header-text">
-          <h1>My Missions</h1>
+          <h2>My Missions</h2>
         </div>
         <table className="mission-table">
           <tbody className="mission-tbody">
@@ -32,6 +37,27 @@ const Profile = () => {
                 <td>You have not joined any mission.</td>
               </tr>
             )}
+          </tbody>
+        </table>
+      </div>
+
+      <div>
+        <div>
+          <h2>My Rockets</h2>
+        </div>
+        <table>
+          <tbody>
+            {
+              reservedRockets.length ? reservedRockets.map((rocket) => (
+                <tr key={rocket.id}>
+                  <td>{rocket.name}</td>
+                </tr>
+              )) : (
+                <tr>
+                  <td>You have not reserved any rocket.</td>
+                </tr>
+              )
+            }
           </tbody>
         </table>
       </div>

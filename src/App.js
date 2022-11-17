8754a496b9b0bc5217158d5d0164a25f4304a855
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-11-17 09:38:41 +0500

    Update the file structure

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import { fetchRocketsData } from './redux/Rockets/rocketSlice';
 import MissionContainer from './components/Missions/MissionContainer';
-import Rockets from './components/rockets/Rockets';
+import Rockets from './components/Rockets/Rockets';
 import Navbar from './components/Navbar';
 import Profile from './components/Profile/Profile';
 
